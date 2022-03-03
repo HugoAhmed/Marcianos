a9321f361bb21ce2d6c40dd266a47fa717f58d5b
enti <enti@enti.enti>
2022-03-03 21:15:36 +0100

    Imagen enemiga insertada

---
+++ index.js
@@ -0,0 +1,111 @@
+#!/usr/bin/node
+
+let http = require("http");
+let fs = require("fs");
+
+
+let http_server = http.createServer(function(req, res)
+{
+
+	if (req.url == "/background.png")
+	{
+		fs.readFile("background.png", function(err, data)
+		{
+			if (err)
+			{
+				console.log("ERROR");
+				return
+			}
+			res.writeHead(200);
+			res.end(data);
+		});
+		return
+	}
+	
+	if (req.url == "/character.png")
+	{
+		fs.readFile("character.png", function(err, data)
+		{
+			if(err)
+			{
+				console.log("ERROR");
+				return
+			}
+			res.writeHead(200);
+			res.end(data);
+		});
+		return
+	}
+
+
+	if (req.url == "/enemy_ship.png")
+	{
+		fs.readFile("enemy_ship.png", function(err, data)
+		{
+			if (err)
+			{
+				console.log("ERROR");
+				return
+			}
+			res.writeHead(200);
+			res.end(data);
+		});
+		return
+	}
+
+	if (req.url == "/car_blue.jpg")
+	{
+		fs.readFile("car_blue.jpg", function (err, data)
+		{
+			if (err)
+			{
+				console.log("ERROR");
+				return
+			}
+		res.writeHead(200);
+		res.end(data);
+		});
+		return
+	}
+
+	if (req.url == "/car_green.jpg")
+	{
+		fs.readFile("car_green.jpg", function (err, data)
+		{
+			if (err)
+			{
+				console.log("ERROR");
+				return
+			}
+		res.writeHead(200);
+		res.end(data);
+		});
+		return
+	}
+
+	if (req.url == "/car_red.jpg")
+	{
+		fs.readFile("car_red.jpg", function (err, data)
+		{
+			if (err)
+			{
+				console.log("ERROR");
+				return
+			}
+			res.writeHead(200);
+			res.end(data);
+		});
+		return
+	}
+
+	fs.readFile("index.html", function (err, data)
+	{
+		if(err)
+		{
+			console.log("ERROR");
+			return
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
+}).listen(1085)
